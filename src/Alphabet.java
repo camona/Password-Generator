432e5381123fbe47f630f679728057c2cfcd8639
Caroline Moland Naterstad <103410211+camona@users.noreply.github.com>
2023-12-09 16:56:43 +0100

    Add fields, constructor and getAlphabet() to Alphabet class

--- src/Alphabet.java
+++ src/Alphabet.java
@@ -1,2 +1,28 @@
 public class Alphabet {
+    private static final String UPPERCASE_LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String LOWERCASE_LETTERS = "abcdefghijklmnopqrstuvwxyz";
+    private static final String NUMBERS = "0123456789";
+    private static final String SYMBOLS = "!@#$%^&*()-_=+/~?";
+
+    private final StringBuilder storage;
+
+    public Alphabet(boolean uppercaseIncluded, boolean lowercaseIncluded, boolean numbersIncluded, boolean symbolsIncluded) {
+        storage = new StringBuilder();
+
+        if (uppercaseIncluded)
+            storage.append(UPPERCASE_LETTERS);
+
+        if (lowercaseIncluded)
+            storage.append(LOWERCASE_LETTERS);
+
+        if (numbersIncluded)
+            storage.append(NUMBERS);
+
+        if (symbolsIncluded)
+            storage.append(SYMBOLS);
+    }
+
+    public String getAlphabet() {
+        return storage.toString();
+    }
 }
