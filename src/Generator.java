f7c5b25be939841d4d58c4862536aaa90caf748a
Caroline Moland Naterstad <103410211+camona@users.noreply.github.com>
2023-12-10 15:21:14 +0100

    Setup of methods

--- src/Generator.java
+++ src/Generator.java
@@ -1,2 +1,42 @@
+import java.util.Scanner;
+
 public class Generator {
+    Alphabet alphabet;
+    private static Scanner keyboard;
+
+    public Generator(boolean includeUpper, boolean includeLower, boolean indcludeNum, boolean indcludeSymb) {
+        alphabet = new Alphabet(includeUpper, includeLower, indcludeNum, indcludeSymb);
+    }
+
+    public void mainLoop() {
+
+    }
+
+    private Password generatePassword(int length) {
+
+    }
+
+    private void printUsefulInfo() {
+
+    }
+
+    private void requestPassword() {
+
+    }
+
+    private boolean isInclude(String input) {
+
+    }
+
+    private void passwordRequestError(String i) {
+
+    }
+
+    private void checkPassword() {
+
+    }
+
+    private void printQuitMessage() {
+
+    }
 }
