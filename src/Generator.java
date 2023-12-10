38065a18c0756da18b64fdf7df915c96f7211af8
Caroline Moland Naterstad <103410211+camona@users.noreply.github.com>
2023-12-10 22:22:45 +0100

    Finish some methods in Generator class

--- src/Generator.java
+++ src/Generator.java
@@ -33,10 +33,26 @@ public class Generator {
     }
 
     private void checkPassword() {
+        String input;
 
+        System.out.println("\nEnter your password:");
+        input = keyboard.next();
+
+        final Password password = new Password(input);
+
+        System.out.println(password.evaluatePassword());
+    }
+
+    private void printMenu() {
+        System.out.println();
+        System.out.println("Enter 1 - Password Generator");
+        System.out.println("Enter 2 - Password Strength Check");
+        System.out.println("Enter 3 - Useful Information");
+        System.out.println("Enter 4 - Quit");
+        System.out.println("Choice:");
     }
 
     private void printQuitMessage() {
-
+        System.out.println("Closing the program.");
     }
 }
