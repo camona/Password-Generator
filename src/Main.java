a600d2912a2a084816af2c355539cddf3d1ad436
Caroline Moland Naterstad <103410211+camona@users.noreply.github.com>
2023-12-16 23:25:26 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Main {
+
+    public static final Scanner keyboard = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        Generator generator = new Generator(keyboard);
+        generator.mainLoop();
+        keyboard.close();
     }
 }
